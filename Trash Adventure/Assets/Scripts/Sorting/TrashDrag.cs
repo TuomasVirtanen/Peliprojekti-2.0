ed8d6b7d22dd2e20c7a7e48b02dbaba058af5fd9
Tuomas <tuomas.p.virtanen@tuni.fi>
2022-04-06 18:54:14 +0300

    Lajittelun pitäisi toimia nyt myös sormella puhelimella. (Ei ole kokeiltu)

--- Trash Adventure/Assets/Scripts/Sorting/TrashDrag.cs
+++ Trash Adventure/Assets/Scripts/Sorting/TrashDrag.cs
@@ -4,14 +4,77 @@ using UnityEngine;
 
 public class TrashDrag : MonoBehaviour
 {
-    void OnMouseDrag()
+    private bool IsDragActive = false;
+    private Vector2 ScreenPosition;
+    private Vector3 WorldPosition;
+    private Draggable LastDragged;
+
+    void Awake()
     {
-        transform.position = GetMousePos();
+        TrashDrag[] controllers = FindObjectsOfType<TrashDrag>();
+        if(controllers.Length > 1)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    Vector3 GetMousePos() {
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
-        return mousePos;
+    void Update()
+    {
+        if(IsDragActive && (Input.GetMouseButtonUp(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)))
+        {
+            Drop();
+            return;
+        }
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            ScreenPosition = new Vector2(mousePos.x, mousePos.y);
+        }
+        else if(Input.touchCount > 0)
+        {
+            ScreenPosition = Input.GetTouch(0).position;
+        }
+        else 
+        {
+            return;
+        }
+
+        WorldPosition = Camera.main.ScreenToWorldPoint(ScreenPosition);
+
+        if(IsDragActive)
+        {
+            Drag();
+        }
+        else 
+        {
+            RaycastHit2D hit = Physics2D.Raycast(WorldPosition, Vector2.zero);
+            if(hit.collider != null)
+            {
+                Draggable draggable = hit.transform.gameObject.GetComponent<Draggable>();
+                if(draggable != null)
+                {
+                    LastDragged = draggable;
+                    InitDrag();
+                }
+            }
+        }
+    }
+
+    void InitDrag()
+    {
+        IsDragActive = true;
+    }
+
+    void Drag()
+    {
+        if(LastDragged != null)
+        {
+            LastDragged.transform.position = new Vector2(WorldPosition.x, WorldPosition.y);
+        }
+    }
+
+    void Drop()
+    {
+        IsDragActive = false;
     }
 }
