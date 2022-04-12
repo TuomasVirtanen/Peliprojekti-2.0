1cb9663e614ed20eff2a97c16126d763a6f3b9a4
Aaro Varjonen <90607504+Avsword@users.noreply.github.com>
2022-04-12 13:59:48 +0300

    lajittelu scenen muunnoksia

--- Trash Adventure/Assets/Scripts/Sorting/CardboardSorting.cs
+++ Trash Adventure/Assets/Scripts/Sorting/CardboardSorting.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class CardboardSorting : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Oikein kierrätetyn efekti")]
+    private ParticleSystem oikea;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Cardboard" || other.tag == "CoffeeMugTrash")
         {
             Debug.Log("Correct");
+
+            Instantiate(oikea, transform.position, Quaternion.identity);
             Destroy(other.gameObject);
         }
         else
